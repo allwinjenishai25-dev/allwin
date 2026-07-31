49028ebe745b762160ecbe13981f370e3b9cb5d0
allwinjenishai25-dev <allwinjenish.ai25@stellamaryscoe.edu.in>
2026-07-31 13:35:00 +0530

    Add files via upload

---
+++ 34.py
@@ -0,0 +1,22 @@
+a=[10,20,30,3,6,20]
+
+print(len(a))
+print(sum(a))
+print(max(a))
+print(min(a))
+print(sorted(a))
+print(sorted(a,reverse=True))
+names=["John","Alice","Bob"]
+marks=[90,80,85]
+students=list(zip(names,marks))
+print(students)
+fruits=["apple","banana","orrange"]
+for index,fruit in enumerate(fruits):
+ print(index,fruit)
+print(pow(2,4))
+print(abs(-10))
+print(round(3.14159,2))
+print(divmod(10,3))
+a=10
+print(type(a))
+print(isinstance(a,int))
